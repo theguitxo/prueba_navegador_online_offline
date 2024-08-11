5a72d45c3df64ffcbe5c1f8968ad5c93b13b97e4
Amador (Guitxo) <guitxo@guitxo.com>
2024-08-12 00:37:21 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'prueba_capacitor' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('prueba_capacitor');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, prueba_capacitor');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NgIf } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [NgIf]
 })
-export class AppComponent {
-  title = 'prueba_capacitor';
+export class AppComponent implements OnInit, OnDestroy {
+  connected!: boolean;
+
+  ngOnInit(): void {
+    this.connected = navigator.onLine;
+    window.addEventListener('offline', () => this.connected = false);
+    window.addEventListener('online', () => this.connected = true);
+  }
+
+  ngOnDestroy(): void {
+      window.removeEventListener('offline', () => {});
+      window.removeEventListener('online', () => {});
+  }
 }
